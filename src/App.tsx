afb17c600ca3ca6cc4c988c5b8787b49c21f232d
Keven <kevenbrito@rede.ulbra.br>
2025-10-15 21:27:02 -0300

    Revert "Keven Adição de três botões."

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,20 @@ function App() {
 
   return (
     <>
+      
       <div>
         <h2>Kevin colocou os botões (O cara é brabo).</h2>
       </div>
 
       <div>
-        <button>
-          Fiz
-        </button>
-      </div>
-
-      <div>
         <h2>João escolheu e colocou a imagem (Sempre tenta fazer uma graça).</h2>
       </div>
-      <div>
-        <button>
-          3
-        </button>
-      </div>
-        
+      
       <div>
         <h2>Matheus analisou friamente todo o projeto (O cara é gelado).</h2>
       </div>
-      <div>
-        <button>
-          Botões
-        </button>
-      </div>
+
+      
     </>
   )
 }
